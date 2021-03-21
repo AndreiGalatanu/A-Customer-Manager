7ea7f2fd4e191bc32cc53dd18518ade5619d610e
Andrei <galatanuandreirafael@gmail.com>
2021-03-21 14:43:21 +0200

    add test

--- Tests/ACM.BLTest/CustomerRepositoryTest.cs
+++ Tests/ACM.BLTest/CustomerRepositoryTest.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using ACM.BL;
+using System.Collections.Generic;
+
 namespace ACM.BLTest
 {
     [TestClass]
@@ -31,5 +33,62 @@ namespace ACM.BLTest
             Assert.AreEqual(expected.EmailAddress, actual.EmailAddress);
 
         }
+
+        [TestMethod]
+        public void RetrieveexistingWithAddress()
+        {
+            //--Arrage
+            var customerRepository = new CustomerRepository();
+            var expected = new Customer(1)
+            {
+
+
+                EmailAddress = "fd@damsd.co",
+                FirstName = "Frodo",
+                LastName = "Baggins",
+                AddressList = new List<Address>()
+                {
+
+                    new Address()
+                    {
+                        AdressType = 1,
+                        StreetLine1 = "Bag end",
+                        StreetLine2 = "Bagshot row",
+                        City = "Hobbiton",
+                        StateProvince = "Shire",
+                        Country = "Middle Earth",
+                        PostalCode = "144"
+                    },
+                    new Address()
+                    {
+                        AdressType = 2,
+                        StreetLine1 = "Bag end",
+                        StreetLine2 = "Dragons end",
+                        City = "Hobsol",
+                        StateProvince = "Srere",
+                        Country = "Mordor",
+                        PostalCode = "1234551"
+
+                    }
+
+                }
+
+            };   //--Act
+            var actual = customerRepository.Retrieve(1);
+
+            //-- Assert
+            Assert.AreEqual(expected.CustomerId, actual.CustomerId);
+            Assert.AreEqual(expected.EmailAddress, actual.EmailAddress);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            for (int i = 0; i < 1; i++)
+            {
+                Assert.AreEqual(expected.AddressList[i].AdressType, actual.AddressList[i].AdressType);
+
+            }
+
+        }
+
+
+
     }
 }
